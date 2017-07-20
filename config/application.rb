07a100e082f4759e66412157a40e3c0ba481799f
Manuel Fittko <manuel@fittko.com>
2017-07-20 15:29:07 +0200

    change default template engine to slim

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
-gsub_file "config/application.rb",
-          "# config.time_zone = 'Central Time (US & Canada)'",
-          'config.time_zone = "Pacific Time (US & Canada)"'
-
 insert_into_file "config/application.rb", :before => /^  end/ do
   <<-'RUBY'
 
     # Ensure non-standard paths are eager-loaded in production
     # (these paths are also autoloaded in development mode)
-    # config.eager_load_paths += %W(#{config.root}/lib)
+    # config.eager_load_paths += %W(#{config.root}/lib)     
+    config.generators do |g|
+      g.template_engine :slim
+    end
+
   RUBY
 end
